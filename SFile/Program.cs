d9a7696f9098931de4fd70dbf175054e811a5f0c
imxingquan <402059985@qq.com>
2016-11-25 01:07:39 +0800

    Add files via upload

---
+++ SFile/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace SFile
+{
+    public struct Param
+    {
+        public string[] args;
+        public string temp_dir;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                //string temp = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory,"temp");
+                //if (!Directory.Exists(temp)) Directory.CreateDirectory(temp);
+                if (args.Length < 4)
+                {
+                    Console.WriteLine("语法:\n   SFile.exe c:\\temp *.txt searchkey log.txt -del");
+                    return;
+                }
+                
+                Param p;
+                p.args = args;
+                p.temp_dir = "";
+
+                Thread th = new Thread(Program.find);
+                th.Start(p);
+
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                Console.WriteLine(UAEx.Message);
+            }
+            catch (PathTooLongException PathEx)
+            {
+                Console.WriteLine(PathEx.Message);
+            }
+            catch (Exception ex) { }
+            finally { }
+            //Console.ReadKey();
+        }
+
+        //public static List<string> ReadFile(string file)
+        //{
+        //    List<string> list = new List<string>();
+        //    StreamReader sr = new StreamReader(file);
+        //    int i = 0;
+        //    while (sr.Peek() > 0)
+        //    {
+        //        list.Add(sr.ReadLine());
+        //        if (++i > 5)
+        //            break;
+        //    }
+        //    sr.Close();
+        //    return list;
+
+        //}
+
+        public static void find(object key)
+        {
+            Param p = (Param)key;
+            string[] t = key.ToString().Split(new char[] { ',' });
+
+            var files = from file in Directory.EnumerateFiles(p.args[0],
+                            p.args[1], SearchOption.AllDirectories)
+                        from line in File.ReadLines(file)
+                        where line.Contains(p.args[2]) && File.GetLastWriteTime(file).Date == DateTime.Now.Date
+                        select new
+                        {
+                            File = file,
+                            Line = line
+                        };
+            List<string> delfiles = new List<string>();
+            foreach (var f in files)
+            {
+                delfiles.Add(f.File);
+                //Console.WriteLine("{0}\t{1}", f.File, f.Line);
+            }
+
+            log(delfiles, p);
+            //Console.WriteLine("{0} files found.",
+            //    files.Count().ToString());
+        }
+
+        public static void log(List<string> files,Param p)
+        {
+            StreamWriter sw = new StreamWriter(p.args[3], true);
+            sw.WriteLine("查找根目录{0} 文件扩展名{1} 包含文本\"{2}\",本次删除{3}个文件:", p.args[0], p.args[1], p.args[2], files.Count);
+            
+            foreach (var item in files)
+            {
+                string msg = string.Format("[{0}] 找到文件: {1}", DateTime.Now, item);
+                sw.WriteLine(msg);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(msg);
+                if (p.args.Length >= 5)
+                {
+                    if (p.args[4] == "-del")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("删除..." + item);
+                        File.Delete(item);
+                    }
+                }
+
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            sw.WriteLine("========================漂亮的分割线==============================");
+            sw.WriteLine();
+            sw.Close();
+
+        }
+    }
+}
---
+++ SFile2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Linq;
+
+namespace SFile
+{
+    public struct Param
+    {
+        public string path;
+        public string pattern;
+        public string oldstr;
+        public string newstr;
+
+        public string SRC_ENCODING;
+        public string DEST_ENCODING;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 6)
+                {
+                    Console.WriteLine("文件编码批量转换语法:\n   et.exe c:\\temp *.txt old new src_encoding dest_encoding \n\t et.exe c:/temp *.txt hi hello utf-8 gb2312");
+                    return;
+                }
+                
+                Param p = new Param();
+                p.path = args[0];
+                p.pattern = args[1];
+                p.oldstr = args[2];
+                p.newstr = args[3];
+                p.SRC_ENCODING = args[4];
+                p.DEST_ENCODING = args[5];
+
+                Thread th = new Thread(Program.run);
+                th.Start(p);
+
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                Console.WriteLine(UAEx.Message);
+            }
+            catch (PathTooLongException PathEx)
+            {
+                Console.WriteLine(PathEx.Message);
+            }
+            catch (Exception ex) { }
+            finally { }
+            //Console.ReadKey();
+        }
+
+        //public static List<string> ReadFile(string file)
+        //{
+        //    List<string> list = new List<string>();
+        //    StreamReader sr = new StreamReader(file);
+        //    int i = 0;
+        //    while (sr.Peek() > 0)
+        //    {
+        //        list.Add(sr.ReadLine());
+        //        if (++i > 5)
+        //            break;
+        //    }
+        //    sr.Close();
+        //    return list;
+
+        //}
+
+        List<string> fileList = new List<string>();
+
+        public static string[] EnumerateFiles(string path,string searchPattern)
+        {
+            return Directory.GetFiles(path, searchPattern, SearchOption.AllDirectories);
+        }
+
+
+        public static void run(object key)
+        {
+            Param p = (Param)key;
+            string[] files = EnumerateFiles(p.path, p.pattern);
+            Encoding srcEncode = Encoding.GetEncoding(p.SRC_ENCODING);
+            Encoding destEncode = Encoding.GetEncoding(p.DEST_ENCODING);
+
+            foreach (var item in files)
+            {
+                StreamReader sr = new StreamReader(item,srcEncode);
+                string c = sr.ReadToEnd();
+                sr.Close();
+                
+                //byte[] srcb = srcEncode.GetBytes(c);
+                //byte[] destb = Encoding.Convert(srcEncode, destEncode, srcb);
+                //c = destEncode.GetString(destb);
+                c = c.Replace(p.oldstr, p.newstr);
+                              
+                StreamWriter sw = new StreamWriter(item, false,destEncode);
+                sw.Write(c);
+                sw.Close();
+
+                //byte[] b = File.ReadAllBytes(item);
+                //byte[] nb = Encoding.Convert(Encoding.GetEncoding(p.SRC_ENCODING), Encoding.GetEncoding(p.DEST_ENCODING), b);
+                //File.WriteAllBytes(item, nb);
+            }
+        }
+
+
+        public static void run2(object key)
+        {
+            Param p = (Param)key;
+            string[] files = EnumerateFiles(p.path, p.pattern);
+            Encoding srcEncode = Encoding.GetEncoding(p.SRC_ENCODING);
+            Encoding destEncode = Encoding.GetEncoding(p.DEST_ENCODING);
+
+            foreach (var item in files)
+            {
+                byte[] srcb = File.ReadAllBytes(item);
+                byte[] destb = Encoding.Convert(Encoding.ASCII, Encoding.UTF8, srcb);
+                string c = destEncode.GetString(destb);
+                c = c.Replace(p.oldstr, p.newstr);
+
+                File.WriteAllText(item, c);
+            
+            }
+        }
+    }
+}
